7094802b7dbf4f465229b4d67d047b7171a6d6a8
KubotaKoki <cottamoci@gmail.com>
2025-03-11 16:59:57 +0900

    テストデータを更新

---
+++ src/mock/data/teamPlayers.ts
@@ -0,0 +1,66 @@
+import type { TeamPlayersLog } from '@/types'
+
+export const teamPlayersLog: TeamPlayersLog[] = [
+  {
+    teamId: '694',
+    players: [
+      {
+        id: '1',
+        name: '笹山陸',
+        alias: '笹山',
+        number: 2,
+        position: 'PG',
+        photo: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '2',
+        name: 'Damien Inglis',
+        alias: 'ダミアン',
+        number: 4,
+        position: 'PG',
+        photo: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '3',
+        name: 'Gary Clark',
+        alias: 'ゲイリー',
+        number: 8,
+        position: 'PG',
+        photo: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '4',
+        name: '大庭岳輝',
+        alias: '大庭',
+        number: 14,
+        position: 'PG',
+        photo: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '5',
+        name: 'Kiefer Ravena',
+        alias: 'キーファー',
+        number: 15,
+        position: 'PG',
+        photo: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '6',
+        name: '森井健太',
+        alias: '森井',
+        number: 18,
+        position: 'PG',
+        photo: 'https://via.placeholder.com/150',
+      },
+      {
+        id: '7',
+        name: 'Maik Kotsar',
+        alias: 'マイク',
+        number: 25,
+        position: 'C',
+        photo: 'https://via.placeholder.com/150',
+      },
+    ],
+    createdAt: '2024-01-01',
+  },
+]
