79d9bd4aad8f8211caf450d889f43868f6c7c3e2
KubotaKoki <cottamoci@gmail.com>
2025-05-29 14:25:55 +0900

    念の為API_KEYを変更してenv化

--- src/firebase_settings/index.ts
+++ src/firebase_settings/index.ts
@@ -11,13 +11,13 @@ import { getFunctions, httpsCallable } from 'firebase/functions'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyA52G6LsA-NvRVcKuOWI8U20mEqhdZBpWA',
-  authDomain: 'corlog-1364e.firebaseapp.com',
-  projectId: 'corlog-1364e',
-  storageBucket: 'corlog-1364e.firebasestorage.app',
-  messagingSenderId: '330748980231',
-  appId: '1:330748980231:web:eb7512b49efc17aadccea2',
-  measurementId: 'G-QGNEF5J6R3',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
